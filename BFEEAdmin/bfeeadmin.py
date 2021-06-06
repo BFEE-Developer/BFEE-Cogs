83b240bddd532bd2555c5f237ce864008ed85aed
Kaktus <daskaktus@gmail.com>
2021-06-06 21:57:00 +0200

    Update bfeeadmin.py

--- BFEEAdmin/bfeeadmin.py
+++ BFEEAdmin/bfeeadmin.py
@@ -13,10 +13,25 @@ class BFEEAdmin(commands.Cog):
     }
     
     def __init__(self, bot):
-        self.bot = bot
-        
+        self.bot = bot        
         self.config = Config.get_conf(self, identifier=13378427487733784274, force_registration=True)
         self.config.register_guild(**self.default_guild)
+        
+    @commands.command(name="lockserver")
+    @checks.mod_or_permissions(manage_messages=True)
+    @commands.guild_only()
+    async def _lockserver(self,ctx):
+        """Lockdown the server"""
+        await self._lockdown(ctx)
+        await ctx.send("Server is now in LOCKDOWN!.")
+        
+    @commands.command(name="unlockserver")
+    @checks.mod_or_permissions(manage_messages=True)
+    @commands.guild_only()
+    async def _unlockserver(self,ctx):
+        """End the lockdown"""
+        await self._unlock(ctx)
+        await ctx.send("Lockdown has stopped.")
     
     @commands.group()
     @checks.admin()
@@ -25,47 +40,69 @@ class BFEEAdmin(commands.Cog):
         """Locksettings configuration commands."""
         pass
         
-    @locksettings.group()
+    @locksettings.command(name="addrole")
     @checks.admin()
     @commands.guild_only()
-    async def addrole(self, ctx, role: discord.Role = None):
+    async def _addrole(self, ctx, role: discord.Role = None):
         """Add role to lockdown."""
         if role is None:
             return
-        if role.id not in await self.blockroles(ctx.guild):
+        if role.id not in await self._get_block_roles(ctx.guild):
             await self._add_block_role(ctx.guild, role.id)
             await ctx.send("Role added")
         else:
-            await ctx.send("Role already blacklisted")
+            await ctx.send("Role already in list")
             
-    @locksettings.group()
+    @locksettings.command(name="delrole")
     @checks.admin()
     @commands.guild_only()
-    async def delrole(self, ctx, role: discord.Role = None):
+    async def _delrole(self, ctx, role: discord.Role = None):
         """Remove role from lockdown."""
         if role is None:
             return
-        if role.id not in await self.blockroles(ctx.guild):
+        if role.id not in await self._get_block_roles(ctx.guild):
             await ctx.send("Role not in list")
         else:
             await self._del_block_role(ctx.guild, role.id)
             await ctx.send("Role removed")
     
-    locksettings.group()
+    @locksettings.command(name="listroles")
     @checks.admin()
     @commands.guild_only()
-    async def listroles(self, ctx):
+    async def _listroles(self, ctx):
         """Lists roles in lockdown."""
         emb = discord.Embed()
         emb.title = "List of roles who will be lockdowned."
         emb.description = "These roles will be affected by the lockdown command."
-        roles = await self.blockroles(ctx.guild)
+        roles = await self._get_block_roles(ctx.guild)
         if not len(roles):
             return await ctx.send("No roles configured")
         emb.add_field(
             name="Roles:", value="\n".join([ctx.guild.get_role(x).mention for x in roles])
         )
         await ctx.send(embed=emb)
+        
+    async def _lockdown(self, ctx):
+        permissions = discord.Permissions()
+        permissions.update(add_reactions = False)
+        permissions.update(send_messages = False)
+        roles = await self._get_block_roles(ctx.guild)
+        if not len(roles):
+            return await ctx.send("No roles configured")
+        else:
+            for x in roles:
+                await ctx.guild.get_role(x).edit(reason = None, permissions=permissions)
+
+    async def _unlock(self, ctx):
+        permissions = discord.Permissions()
+        permissions.update(add_reactions = True)
+        permissions.update(send_messages = True)
+        roles = await self._get_block_roles(ctx.guild)
+        if not len(roles):
+            return await ctx.send("No roles configured")
+        else:
+            for x in roles:
+                await ctx.guild.get_role(x).edit(reason = None, permissions=permissions)
             
     async def _add_block_role(self, guild, role):
         async with self.config.guild(guild).blockroles() as rolelist:
@@ -74,6 +111,9 @@ class BFEEAdmin(commands.Cog):
     async def _del_block_role(self, guild, role):
         async with self.config.guild(guild).blockroles() as rolelist:
             rolelist.remove(role)  
+    
+    async def _get_block_roles(self, guild):
+        return await self.config.guild(guild).blockroles()
     
     # permissions = discord.Permissions()
     # permissions.update(kick_members = False)
